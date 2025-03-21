e256e580eb38a8accd9e0e556c8bcdb119aed3d7
amdocsprasoon <prasoonv@amdocs.com>
2025-03-21 15:18:55 +0530

    First commit

---
+++ src/main/java/org/sebprojects/aspectop/AspectOpApplication.java
@@ -0,0 +1,13 @@
+package org.sebprojects.aspectop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectOpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AspectOpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sebprojects/aspectop/AspectOpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebprojects.aspectop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectOpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
